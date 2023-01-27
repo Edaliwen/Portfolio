5544745c0831306e10a981d81480a6987413ec2e
Edaliwen <amandine.valtat@gmail.com>
2023-01-27 16:44:54 +0100

    Gestion de l'affichage des projets
    Utilisation d'une card bootstrap pour afficher les champ de la table project
    + ajout du lien bootstrap pour afficher les icônes

--- projets.php
+++ projets.php
@@ -2,15 +2,45 @@
 // Commencer par l'appel du controller
 require("./controllers/projectController.php");
 
+$controller = new ProjectController;
+$projects = $controller->readAll();
+
 define("PAGE_TITLE", "Projets");
 
 ?>
+
 <?php include("./assets/inc/head.php"); ?>
 <?php include("./assets/inc/header.php"); ?>
 
 <main>
     <div class="container-fluid">
-        
+        <h1>Mes projets</h1>
+        <div class="row">
+        <?php
+            // var_dump($projects);
+            foreach($projects as $project){
+        ?>   
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $project->name ?></h5>
+                        <p class="card-text"><?= $project->description ?></p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Date de début: <?= $project->date_start ?></li>
+                        <li class="list-group-item">Date de fin: <?= $project->date_end ?></li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="<?= $project->link_site ?>" class="card-link">Lien vers le projet</a>
+                        <a href="<?= $project->link_site ?>" class="card-link"><i class="bi bi-github"></i></a>
+                    </div>
+                </div>
+            </div>
+        <?php
+            }
+        ?>
+        </div>        
     </div>
 </main>
 
