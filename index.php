1fbd0017529c57973698de6a13bc25851edcba5d
Edaliwen <amandine.valtat@gmail.com>
2023-01-27 11:48:19 +0100

    First commit
    Mise en place de la structure du site

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// Commencer par l'appel du controller
+// require(controllers/...)
+
+define("PAGE_TITLE", "Accueil");
+?>
+<?php include("./assets/inc/head.php"); ?>
+<?php include("./assets/inc/header.php"); ?>
+
+<main>
+    
+</main>
+
+<?php include("./assets/inc/footer.php"); ?>
